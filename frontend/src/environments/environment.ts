bb182c6179a95598a9d0f7b182488fadbbdfca9e
DomQuangMinhTriet <dqmtriet23@clc.fitus.edu.vn>
2026-02-03 21:35:22 +0700

    feat(config)[#4]: Setup supabase, resend, cloudinary and vietqr

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api/v1',
-  supabaseUrl: 'https://your-project.supabase.co',
-  supabaseAnonKey: 'your_anon_key'
+  supabaseUrl: 'https://hyijalbbdxovyblyscxm.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5aWphbGJiZHhvdnlibHlzY3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMTg1NTYsImV4cCI6MjA4NTY5NDU1Nn0.-0Y0MXUIWa3Y4Y9pNWf0tV_CYBA4N8kuyIXzjy6QBfU'
 };
