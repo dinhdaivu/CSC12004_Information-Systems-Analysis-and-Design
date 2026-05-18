0be48aef6b07d20768cc24692246de3d05d062c2
DomQuangMinhTriet <dqmtriet23@clc.fitus.edu.vn>
2026-05-18 23:19:09 +0700

    Update Environments for Production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
+  production: true,
+  apiUrl: 'https://brown-website-production.up.railway.app/api',
+  socketUrl: 'https://brown-website-production.up.railway.app',
   supabaseUrl: 'https://hyijalbbdxovyblyscxm.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5aWphbGJiZHhvdnlibHlzY3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMTg1NTYsImV4cCI6MjA4NTY5NDU1Nn0.-0Y0MXUIWa3Y4Y9pNWf0tV_CYBA4N8kuyIXzjy6QBfU'
 };
