c6c26956835d4410663ffb16c9198e928970318b
DomQuangMinhTriet <dqmtriet23@clc.fitus.edu.vn>
2026-05-18 23:24:00 +0700

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://brown-website-production.up.railway.app/api',
-  socketUrl: 'https://brown-website-production.up.railway.app',
+  apiUrl: 'https://backend-production-2484.up.railway.app/api',
+  socketUrl: 'https://backend-production-2484.up.railway.app',
   supabaseUrl: 'https://hyijalbbdxovyblyscxm.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5aWphbGJiZHhvdnlibHlzY3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMTg1NTYsImV4cCI6MjA4NTY5NDU1Nn0.-0Y0MXUIWa3Y4Y9pNWf0tV_CYBA4N8kuyIXzjy6QBfU'
 };
