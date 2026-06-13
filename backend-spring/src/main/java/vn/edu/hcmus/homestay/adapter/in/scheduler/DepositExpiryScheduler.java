80e562bde9adf808cf26e7add68936b265025b98
RainyinSaiGon <dinhdaivu1109@gmail.com>
2026-06-13 11:29:10 +0700

    Fix asynce error

--- backend-spring/src/main/java/vn/edu/hcmus/homestay/adapter/in/scheduler/DepositExpiryScheduler.java
+++ backend-spring/src/main/java/vn/edu/hcmus/homestay/adapter/in/scheduler/DepositExpiryScheduler.java
@@ -14,6 +14,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
 import vn.edu.hcmus.homestay.application.port.out.identity.LoadUserPort;
 import vn.edu.hcmus.homestay.application.port.out.rental.LoadDepositPort;
 import vn.edu.hcmus.homestay.application.port.out.rental.LoadRentalRequestPort;
